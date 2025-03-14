8777b8dd6b8a6598d08d5d0c171825916c46f37c
manoj1234-ms <manojsharma02620@gmail.com>
2025-03-14 23:16:49 +0530

    change backend dir

--- Backend/index.js
+++ Backend/index.js
@@ -16,12 +16,13 @@ const PORT = process.env.PORT || 3000;
 const __dirname  =path.resolve();
 // console.log(__dirname);
 
-app.get("/",(_ ,res) => {
-    return res.status(200).json({
-        message:"I am coming from backend",
-        success: true
-    })
-})
+// app.get("/",(_ ,res) => {
+//     return res.status(200).json({
+//         message:"I am coming from backend",
+//         success: true
+//     })
+// })
+
 //middleware
 app.use(express.json());
 app.use(cookieParser());
