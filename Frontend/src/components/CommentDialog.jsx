0916de5fa92464b84522e5af5a65011ccba0a894
manoj1234-ms <manojsharma02620@gmail.com>
2025-03-14 22:56:37 +0530

    url changes

--- Frontend/src/components/CommentDialog.jsx
+++ Frontend/src/components/CommentDialog.jsx
@@ -34,7 +34,7 @@ const CommentDialog = ({ open, setOpen }) => {
   const sendMessageHandler = async () => {
 
     try {
-      const res = await axios.post(`https://localhost:8000/api/v1/post/${selectedPost?._id}/comment`, { text }, {
+      const res = await axios.post(`https://instagram-clone-kex4.onrender.com/api/v1/post/${selectedPost?._id}/comment`, { text }, {
         headers: {
           'Content-Type': 'application/json'
         },
